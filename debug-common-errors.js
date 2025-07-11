df17443f61dc4d3765baf1d32272fc425198aa18
cmlorbert <cmlorbert@gmail.com>
2025-07-10 22:44:51 -0500

    finished

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,22 +24,22 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp!");
 
-// What’s Wrong?
+// What’s Wrong? -- This is a Syntax Error. It is missing closing quotations and parenthesis.
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
+// What’s Wrong? -- This is a Runtime Error. "eight" should be a number not a string. It returns NaN. Replace "eight" with 8.
 
 
 
@@ -50,7 +50,7 @@ for (let i = 0; i < numbers.length; i++) {
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
+    if (num % i !== 0) {
       return true;  // Supposed to indicate num is NOT prime
     }
   }
@@ -59,4 +59,4 @@ function isPrime(num) {
 
 console.log(isPrime(7)); // Expected true but gets false
 
-// What’s Wrong?
+// What’s Wrong? -- This is a Logic Error.
